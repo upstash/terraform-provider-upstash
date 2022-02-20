15ca95c30e6969113f5493294bc596cf109485f1
yilmaz-burak <bylmaz744@gmail.com>
2022-02-20 18:33:49 +0300

    docs

---
+++ examples/data-sources/upstash_kafka_topic_data/data-source.tf
@@ -0,0 +1,3 @@
+data "upstash_kafka_topic_data" "kafkaTopicData" {
+    topic_id = resource.upstash_kafka_topic.exampleKafkaTopic.topic_id
+}
---
+++ examples/resources/upstash_kafka_cluster/resource.tf
@@ -0,0 +1,5 @@
+resource "upstash_kafka_cluster" "exampleCluster" {
+  cluster_name = "TerraformCluster"
+  region = "eu-west-1"
+  multizone = false
+}
