30685fc2aa37dc08ab38de7a18077c8bb6b0bf51
burak-upstash <burak@upstash.com>
2023-01-23 23:02:11 +0300

    add decoded_username

--- upstash/kafka/credential/types.go
+++ upstash/kafka/credential/types.go
@@ -1,15 +1,16 @@
 package credential
 
 type Credential struct {
-	CredentialId   string `json:"credential_id"`
-	CredentialName string `json:"credential_name"`
-	Topic          string `json:"topic"`
-	Permissions    string `json:"permissions"`
-	ClusterId      string `json:"cluster_id"`
-	Username       string `json:"username"`
-	CreationTime   int64  `json:"creation_time"`
-	State          string `json:"state"`
-	Password       string `json:"password"`
+	CredentialId    string `json:"credential_id"`
+	CredentialName  string `json:"credential_name"`
+	Topic           string `json:"topic"`
+	Permissions     string `json:"permissions"`
+	ClusterId       string `json:"cluster_id"`
+	Username        string `json:"username"`
+	EncodedUsername string `json:"encoded_username"`
+	CreationTime    int64  `json:"creation_time"`
+	State           string `json:"state"`
+	Password        string `json:"password"`
 }
 
 type CreateCredentialRequest struct {
