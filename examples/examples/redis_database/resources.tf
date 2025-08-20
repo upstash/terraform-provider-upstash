4fb2f4f60752bf379d8d69d80461b430d5db0c1e
burak-upstash <burak@upstash.com>
2025-08-21 00:43:30 +0300

    Update Redis database example to use variable for database name and clean up redundant code

--- examples/examples/redis_database/resources.tf
+++ examples/examples/redis_database/resources.tf
@@ -1,23 +1,11 @@
 resource "upstash_redis_database" "exampleDB" {
-  database_name = "tf testa"
+  database_name = var.database_name
   region        = "global"
   tls           = true
   auto_scale    = true
   eviction      = true
   prod_pack     = true
-  budget = 30
-
-  primary_region = "eu-west-1"
-}
-
-resource "upstash_redis_database" "exampleDB" {
-  database_name = var.database_name
-  region        = "global"
-  tls           = true
-  auto_scale    = var.auto_scale
-  eviction      = var.eviction
-  prod_pack     = var.prod_pack
-  budget        = var.budget
+  budget        = 30
 
 
   primary_region = var.primary_region
